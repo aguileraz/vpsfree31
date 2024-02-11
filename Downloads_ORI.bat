9caa9255a4d40c2ffa26adcf090798b629c52c3d
Carlos Aguilera <kakos_look@hotmail.com>
2024-02-11 19:32:33 -0300

    Add files via upload

--- Downloads_ORI.bat
+++ Downloads_ORI.bat
@@ -5,7 +5,7 @@ curl -L -o show.bat "https://www.dropbox.com/scl/fi/cwbwdo2n3tt8rbqmugc6h/show.b
 certutil -urlcache -split -f "https://github.com/rustdesk/rustdesk/releases/download/1.2.3/rustdesk-1.2.3-x86_64.exe" rustdesk.exe
 pip install pyautogui --quiet
 pip install psutil --quiet
-curl -s -L -o time.py "https://raw.githubusercontent.com/aguileraz/vpsfree31/main/timelimit.py"
+curl -s -L -o time.py "https://www.dropbox.com/scl/fi/ox42qglbf6fsnm9erf8cw/timelimit.py?rlkey=opyeqgum1k95kud81xlc7d66r&dl=0"
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
@@ -20,4 +20,4 @@ powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-Secu
 start "" "rustdesk.exe"
 python login.py
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d 0 /f
-tzutil /s "E. South America Standard Time"
+tzutil /s "Sri Lanka Standard Time"
